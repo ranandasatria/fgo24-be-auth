db6af36c995d418e7643c5edf99bd1b71d343ef7
ranandasatria <rananda24@gmail.com>
2025-06-23 21:51:51 +0700

    refactor: add struct and slice of struct for forgot and reset password flow

--- models/user.go
+++ models/user.go
@@ -1,8 +1,23 @@
 package models
 
 type User struct {
-	Email    string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
+	Name     string `form:"name" json:"name"`
+	Email    string `form:"email" json:"email"`
+	Password string `form:"password" json:"password"`
 }
 
-var Users []User
+var Users []*User
+
+type ForgotPass struct {
+	Email   string
+	OTPCode int
+}
+
+var ListForgotPass []*ForgotPass
+
+type Reset struct {
+	Email    string `json:"email" form:"email"`
+	OTP      int    `json:"otp" form:"otp"`
+	NewPass  string `json:"newPass" form:"newPass"`
+	ConfPass string `json:"confPass" form:"confPass"`
+}
